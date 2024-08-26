5285ea0eec51d3371910d3b74e225fff8409e4cb
Diego Pennisi <diego.pennisi@grupoctc.com>
2024-08-26 12:00:58 +0200

    26-08-2024

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   // url: 'http://localhost/CTC/controlHorarioApi/Controller/'
   // 
 
-  url: 'http://92.54.11.138/CTC/Control/Api/controlHorarioApi/Controller/'
+  url: 'https://lecta.grupoctc.com/CTC/Control/Api/controlHorarioApi/Controller/'
 
 };
 
